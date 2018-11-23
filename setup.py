44e10fd773b7d8da55ee16f806124a6dffead607
mayurpatil888 <mayur@ost.com>
2018-11-23 19:31:32 +0530

    package name change

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-setup(name='ost_kyc_sdk',    
+setup(name='ost_kyc_sdk_python',    
       version='2.0',
       description='OST KYC SDK in python',
       url='https://github.com/OpenSTFoundation/kyc-sdk-python',
