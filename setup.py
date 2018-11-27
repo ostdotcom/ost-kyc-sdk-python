69ddc1930988cc89a90200b734d211f71bd2e140
mayurpatil888 <mayur@ost.com>
2018-11-27 17:17:13 +0530

    add version beta in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='ost_kyc_sdk_python',    
-      version='2.0.0',
+      version='2.0.0b',
       description='OST KYC SDK in python',
       url='https://github.com/OpenSTFoundation/kyc-sdk-python',
       packages = find_packages(),
