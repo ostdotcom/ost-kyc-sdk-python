430ce2b21fd13add4351ba1303891661fa2d5b4d
mayurpatil888 <mayur@ost.com>
2018-12-05 14:52:27 +0530

    read long desc

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
+import os 
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()  
+
 setup(name='ost_kyc_sdk_python',    
-      version='2.0.0b',
-      description='OST KYC SDK in python',
+      version = '2.0.0b',
+      description = 'OST KYC SDK in python',
+      long_description = read('README.md'),
       url='https://github.com/OpenSTFoundation/kyc-sdk-python',
       packages = find_packages(),
       install_requires = ['requests'],
