ae46ce9b577588bfcdadf7eabd381a6d9a4b74f7
Shlok Gilda <shlok@ost.com>
2018-12-14 13:41:32 +0530

    Removed default value as argument.

--- ost_kyc_sdk_python/saas/users.py
+++ ost_kyc_sdk_python/saas/users.py
@@ -6,41 +6,40 @@ class Users(Base):
         Base.__init__(self, params)
         self.endpoint = '/api/v2/users'
 
-    # 
-    # Create user 
+    #
+    # Create user
     # * Author: Mayur
     # * Date: 20/11/2018
     # * Reviewed By:
     #
     # Return dict
-    # 
+    #
     def create(self, params):
         return self.http_helper.send_post_request(self.endpoint , params)
 
-    # 
+    #
     # Get user info
     # * Author: Mayur
     # * Date: 20/11/2018
     # * Reviewed By:
     #
     # Return dict
-    # 
-    def get(self, params={}):
+    #
+    def get(self, params = None):
+        params = params or {}
         self.raise_exception_if_param_absent_or_invalid(params, 'id')
         endpoint =  self.endpoint + "/" + str(params.get('id'))
-        self.delete_key_from_params(params, 'id')   
+        self.delete_key_from_params(params, 'id')
         return self.http_helper.send_get_request(endpoint, params)
 
-    # 
+    #
     # List user info
     # * Author: Mayur
     # * Date: 20/11/2018
     # * Reviewed By:
     #
     # Return dict
-    # 
-    def list(self, params={}):
+    #
+    def list(self, params = None):
+        params = params or {}
         return self.http_helper.send_get_request(self.endpoint, params)
-
-
-
